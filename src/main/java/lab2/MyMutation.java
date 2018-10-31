b41a5aacc95123316a12043135010109ed8b58c3
HighExecutor <mihail.melnik.ifmo@mail.ru>
2018-10-31 16:19:54 +0300

    init

---
+++ src/main/java/lab2/MyMutation.java
@@ -0,0 +1,18 @@
+package lab2;
+
+import org.uncommons.watchmaker.framework.EvolutionaryOperator;
+
+import java.util.List;
+import java.util.Random;
+
+public class MyMutation implements EvolutionaryOperator<double[]> {
+    public List<double[]> apply(List<double[]> population, Random random) {
+        // initial population
+        // need to change individuals, but not their number!
+
+        // your implementation:
+
+        //result population
+        return population;
+    }
+}
